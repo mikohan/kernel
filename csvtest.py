c710d3bc80929a4d2eaf102d50a918b744b4e7b1
manhee <angara99@gmail.com>
2018-11-29 11:07:21 +0300

    sendint

--- csvtest.py
+++ csvtest.py
@@ -1,6 +1,17 @@
 import csv
-with open("PorterOlesyaCopy.csv", newline='') as f:
+import MySQLdb as MyCl
+
+db = MyCl.connect('localhost', 'root', 'manhee33338', 'test')
+db.set_character_set('utf8')
+cursor = db.cursor()
+
+with open("engine_cat.csv", newline='') as f:
     reader = csv.reader(f, delimiter =",")
     for row in reader:
+
+        q = "INSERT INTO category_olesya (parent_id, category_name) VALUES(%s,%s)"
+        cursor.execute(q,(row))
+
         print(row)
+    db.commit()
     f.close()
